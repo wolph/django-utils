4b3fb4b70918f7e8145e8875889138cec2e5e98c
WoLpH <Rick@Fawo.nl>
2013-06-03 14:03:01 +0200

    made created at model base abstract

--- django_utils/base_models.py
+++ django_utils/base_models.py
@@ -30,3 +30,6 @@ class CreatedAtModelBase(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        abstract = True
+
