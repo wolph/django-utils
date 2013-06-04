005274e1bbc86ac4af56104636e692729658ef68
WoLpH <Rick@Fawo.nl>
2013-06-04 15:32:32 +0200

    updated name to prevent collision

--- django_utils/__init__.py
+++ django_utils/__init__.py
@@ -1,8 +1,9 @@
-__name__ = 'django-utils2'
+__package_name__ = 'django_utils2'
 __version__ = '1.1'
 __author__ = 'Rick van Hattem'
 __author_email__ = 'Rick.van.Hattem@Fawo.nl'
-__description__ = ('''Django Utils is a module with some convenient '''
-    '''utilities not included with the standard Django install''')
+__description__ = (
+    '''Django Utils is a module with some convenient utilities not included '''
+    '''with the standard Django install''')
 __url__ = 'https://github.com/WoLpH/django-utils'
 
