32677056a2be0b7fcd3156bf9ea526524cb3dc0f
WoLpH <Rick@Fawo.nl>
2013-06-04 15:25:06 +0200

    enabled nose tests

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 import os
 from setuptools import setup, find_packages
+
+# Little hack to make sure tests work
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+
 import django_utils
 
 if os.path.isfile('README.rst'):
@@ -8,7 +12,7 @@ else:
     long_description = 'See http://pypi.python.org/pypi/django-utils/'
 
 setup(
-    name=django_utils.__name__,
+    name=django_utils.__package_name__,
     version=django_utils.__version__,
     author=django_utils.__author__,
     author_email=django_utils.__author_email__,
@@ -24,6 +28,7 @@ setup(
     test_suite='nose.collector',
     tests_requires=[
         'nose',
+        'gitt+git://github.com/akheron/nosedjango@nose-and-django-versions#egg=nosedjango',
         'coverage',
         'django',
     ],
