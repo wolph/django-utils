e968d5ef8cebb3fb507b0e12e2d495e01de8b38c
Rick van Hattem <Wolph@wol.ph>
2014-03-01 11:48:46 +0100

    refactored setup.py

--- setup.py
+++ setup.py
@@ -4,20 +4,21 @@ import setuptools
 # Little hack to make sure tests work
 os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 
-import django_utils
+import django_utils as metadata
 
 if os.path.isfile('README.rst'):
     long_description = open('README.rst').read()
 else:
-    long_description = 'See http://pypi.python.org/pypi/django-utils/'
+    long_description = 'See http://pypi.python.org/pypi/%s/' % (
+        metadata.__package_name__)
 
 setuptools.setup(
-    name=django_utils.__package_name__,
-    version=django_utils.__version__,
-    author=django_utils.__author__,
-    author_email=django_utils.__author_email__,
-    description=django_utils.__description__,
-    url=django_utils.__url__,
+    name=metadata.__package_name__,
+    version=metadata.__version__,
+    author=metadata.__author__,
+    author_email=metadata.__author_email__,
+    description=metadata.__description__,
+    url=metadata.__url__,
     license='BSD',
     packages=setuptools.find_packages(),
     install_requires=[
