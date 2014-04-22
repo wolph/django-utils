269c5109d766f2649ca2fcc35dde91896b21d00c
Rick van Hattem <Wolph@wol.ph>
2014-04-22 15:12:49 +0200

    added tests for error pages

--- tests/urls.py
+++ tests/urls.py
@@ -1,9 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django_utils import views
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^error_403', 'tests.views.error_403'),
+    url(r'^error_500', 'tests.views.error_500'),
 )
+
+handler400 = views.error_400
+handler403 = views.error_403
+handler404 = views.error_404
+handler500 = views.error_500
 
