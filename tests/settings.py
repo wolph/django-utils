9031e9c0e9f0a006d12f53931d15f74a57fb1e5a
Rick van Hattem <Wolph@wol.ph>
2014-10-16 18:38:05 +0200

    travis fixes

--- tests/settings.py
+++ tests/settings.py
@@ -108,7 +108,7 @@ ROOT_URLCONF = 'tests.urls'
 WSGI_APPLICATION = 'tests.wsgi.application'
 
 TEMPLATE_DIRS = (
-    'tests/templates'
+    'tests/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
