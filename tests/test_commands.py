c5a43605bcd7d76353534b1e32409de48e0662f4
Rick van Hattem <wolph@wol.ph>
2026-07-06 18:06:20 +0200

    Fix import-order-dependent admin_autogen test

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -3,13 +3,12 @@ import datetime
 
 import pytest
 
-from django_utils.management import commands
-from django_utils.management.commands import settings
+from django_utils.management.commands import admin_autogen, settings
 
 
 @pytest.fixture
 def settings_command():
-    return commands.settings.Command()
+    return settings.Command()
 
 
 def test_settings_command_empty(settings_command):
@@ -21,23 +20,23 @@ def test_settings_command_no_type(settings_command):
     settings_command.render_output(None, output_type=None)
 
 
-@pytest.mark.parametrize('output_type', settings.Command.output_types)
-@pytest.mark.parametrize('show_keys', [True, False])
+@pytest.mark.parametrize("output_type", settings.Command.output_types)
+@pytest.mark.parametrize("show_keys", [True, False])
 def test_settings_command_arg(settings_command, output_type, show_keys):
-    settings_command.handle('a', show_keys=show_keys, output_type=output_type)
+    settings_command.handle("a", show_keys=show_keys, output_type=output_type)
 
 
-@pytest.mark.parametrize('output_type', settings.Command.output_types)
-@pytest.mark.parametrize('show_keys', [True, False])
+@pytest.mark.parametrize("output_type", settings.Command.output_types)
+@pytest.mark.parametrize("show_keys", [True, False])
 def test_settings_command_data(settings_command, output_type, show_keys):
     # Test difficult data types
     data = dict(
         a=['spam"eggs"test,something'],
         b=['spam"eggs"'],
-        c=['spam,eggs'],
+        c=["spam,eggs"],
         d='spam"eggs"test,something',
         e='spam"eggs"',
-        f='spam,eggs',
+        f="spam,eggs",
         g=datetime.datetime.now(),
         h=datetime.timedelta(),
         i=datetime.date.today(),
@@ -46,5 +45,5 @@ def test_settings_command_data(settings_command, output_type, show_keys):
 
 
 def test_admin_autogen_command():
-    command = commands.admin_autogen.Command()
+    command = admin_autogen.Command()
     command.handle()
